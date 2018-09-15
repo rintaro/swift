b3aead0dda747cf3fc1ea9d6c07ee90b3fcdbcce
Rintaro Ishizaki <rishizaki@apple.com>
2018-09-15 19:35:02 +0900

    [Lexer] Improve fix-it to remove "too long" delimiter in string literal
    Fix-it to remove extra '#'s once.

--- test/Parse/raw_string_errors.swift
+++ test/Parse/raw_string_errors.swift
@@ -8,7 +8,7 @@
 // expected-error@-1{{unterminated string literal}}
 
 ###"invalid"######
-// expected-error@-1{{too many '#' characters in closing delimiter}}{{16-17=}}
+// expected-error@-1{{too many '#' characters in closing delimiter}}{{16-19=}}
 // expected-error@-2{{consecutive statements on a line must be separated by ';'}}
 // expected-error@-3{{expected expression}}
 // expected-warning@-4{{string literal is unused}}
