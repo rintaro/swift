0b7738a47c8120c410eb7a45f46bfd3e3fd7c111
Rintaro Ishizaki <fs.output@gmail.com>
2019-08-16 05:48:25 +0000

    [SyntaxParse] Don't record tokens while skipping

--- lib/Parse/ParseDecl.cpp
+++ lib/Parse/ParseDecl.cpp
@@ -2713,6 +2713,8 @@ bool Parser::isStartOfDecl() {
 void Parser::consumeDecl(ParserPosition BeginParserPosition,
                          ParseDeclOptions Flags,
                          bool IsTopLevel) {
+  SyntaxParsingContext Discarding(SyntaxContext);
+  Discarding.setDiscard();
   SourceLoc CurrentLoc = Tok.getLoc();
 
   SourceLoc EndLoc = PreviousLoc;
