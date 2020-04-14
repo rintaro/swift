893d04d9d4e327c2f3095455555ecfa2c4374062
Rintaro Ishizaki <rishizaki@apple.com>
2020-04-14 15:31:01 -0700

    [Syntax] Update for braceless multiple trailing closure syntax

--- test/Syntax/round_trip_parse_gen.swift
+++ test/Syntax/round_trip_parse_gen.swift
@@ -93,10 +93,7 @@ class C {
   func implictMember() {
     _ = .foo
     _ = .foo(x: 12)
-    _ = .foo { 12 }
-    _ = .foo {
-        arg1: { 12 }
-      }
+    _ = .foo() { 12 }
     _ = .foo[12]
     _ = .foo.bar
   }
@@ -261,28 +258,21 @@ func postfix() {
   foo()
   foo() {}
   foo {}
-  foo {
-    arg1: {}
-  }
-  foo() {
-    arg1: {}
+  foo { }
     arg2: {}
-  }
   foo {}
   foo.bar()
   foo.bar() {}
-  foo.bar() {
-    arg1: {}
-  }
+  foo.bar() {}
+    arg2: {}
+    in: {}
   foo.bar {}
   foo[]
   foo[1]
   foo[] {}
   foo[1] {}
-  foo[1] {
-    arg1: {}
+  foo[1] {}
     arg2: {}
-  }
   foo[1][2,x:3]
   foo?++.bar!(baz).self
   foo().0
