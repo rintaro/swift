fd89db506e2666f576549cb39d3e6070d9027a5d
Michael Gottesman <mgottesman@apple.com>
2026-03-10 08:48:04 -0700

    [flow-isolation] Add support for global-actor-isolated nominal type initializers
    Previously, flow-sensitive isolation checking only applied to actor instance
    initializers. This extends it to cover initializers of global-actor-isolated
    nominal types (structs, classes).
    
    rdar://131136194

--- test/Concurrency/flow_isolation_enum.swift
+++ test/Concurrency/flow_isolation_enum.swift
@@ -55,28 +55,28 @@ enum MainActorEnumWithPayloads {
   nonisolated init(v1: Void) {
     self = .nonSendable(NonSendableType())
 
-    trigger()
+    trigger() // expected-note {{after this use of 'self', only nonisolated properties of 'self' can be accessed from this init}}
 
     _ = self.nonisoComputed
-    self = .nonSendable(NonSendableType())
+    self = .nonSendable(NonSendableType()) // expected-warning {{cannot access 'self' here in nonisolated initializer; this is an error in the Swift 6 language mode; this is an error in the Swift 6 language mode}}
   }
 
   @MainActor init(v2: Void) {
     self = .nonSendable(NonSendableType())
 
-    trigger()
+    trigger() // expected-note {{after this use of 'self', only main actor-isolated properties of 'self' can be accessed from this init}}
 
     _ = self.nonisoComputed
-    self = .nonSendable(NonSendableType())
+    self = .nonSendable(NonSendableType()) // expected-warning {{cannot access 'self' here in main actor-isolated initializer; this is an error in the Swift 6 language mode; this is an error in the Swift 6 language mode}}
   }
 
   @CustomActor init(v3: Void) {
     self = .nonSendable(NonSendableType())
 
-    trigger()
+    trigger() // expected-note {{after this use of 'self', only global actor 'CustomActor'-isolated properties of 'self' can be accessed from this init}}
 
     _ = self.nonisoComputed
-    self = .nonSendable(NonSendableType())
+    self = .nonSendable(NonSendableType()) // expected-warning {{cannot access 'self' here in global actor 'CustomActor'-isolated initializer; this is an error in the Swift 6 language mode; this is an error in the Swift 6 language mode}}
   }
 }
 
@@ -96,28 +96,28 @@ enum MainActorEnumWithPayloads_NC: ~Copyable {
   nonisolated init(v1: Void) {
     self = .nonSendable(NonSendableType())
 
-    trigger()
+    trigger() // expected-note {{after this use of 'self', only nonisolated properties of 'self' can be accessed from this init}}
 
     _ = self.nonisoComputed
-    self = .nonSendable(NonSendableType())
+    self = .nonSendable(NonSendableType()) // expected-warning {{cannot access 'self' here in nonisolated initializer; this is an error in the Swift 6 language mode; this is an error in the Swift 6 language mode}}
   }
 
   @MainActor init(v2: Void) {
     self = .nonSendable(NonSendableType())
 
-    trigger()
+    trigger() // expected-note {{after this use of 'self', only main actor-isolated properties of 'self' can be accessed from this init}}
 
     _ = self.nonisoComputed
-    self = .nonSendable(NonSendableType())
+    self = .nonSendable(NonSendableType()) // expected-warning {{cannot access 'self' here in main actor-isolated initializer; this is an error in the Swift 6 language mode; this is an error in the Swift 6 language mode}}
   }
 
   @CustomActor init(v3: Void) {
     self = .nonSendable(NonSendableType())
 
-    trigger()
+    trigger() // expected-note {{after this use of 'self', only global actor 'CustomActor'-isolated properties of 'self' can be accessed from this init}}
 
     _ = self.nonisoComputed
-    self = .nonSendable(NonSendableType())
+    self = .nonSendable(NonSendableType()) // expected-warning {{cannot access 'self' here in global actor 'CustomActor'-isolated initializer; this is an error in the Swift 6 language mode; this is an error in the Swift 6 language mode}}
   }
 }
 
@@ -138,55 +138,55 @@ enum MainActorEnumWithPayloads_AO<T> {
   nonisolated init(v1: Void, t: sending T) {
     self = .addressOnly(NonSendableAOStruct(value: t))
 
-    trigger()
+    trigger() // expected-note {{after this use of 'self', only nonisolated properties of 'self' can be accessed from this init}}
 
     _ = self.nonisoComputed
-    self = .nonSendable(NonSendableType())
+    self = .nonSendable(NonSendableType()) // expected-warning {{cannot access 'self' here in nonisolated initializer; this is an error in the Swift 6 language mode; this is an error in the Swift 6 language mode}}
   }
 
   nonisolated init?(v1_failable: Void, t: sending T) {
     self = .addressOnly(NonSendableAOStruct(value: t))
 
-    trigger()
+    trigger() // expected-note {{after this use of 'self', only nonisolated properties of 'self' can be accessed from this init}}
 
     _ = self.nonisoComputed
-    self = .nonSendable(NonSendableType())
+    self = .nonSendable(NonSendableType()) // expected-warning {{cannot access 'self' here in nonisolated initializer; this is an error in the Swift 6 language mode; this is an error in the Swift 6 language mode}}
   }
 
   @MainActor init(v2: Void, t: sending T) {
     self = .addressOnly(NonSendableAOStruct(value: t))
 
-    trigger()
+    trigger() // expected-note {{after this use of 'self', only main actor-isolated properties of 'self' can be accessed from this init}}
 
     _ = self.nonisoComputed
-    self = .nonSendable(NonSendableType())
+    self = .nonSendable(NonSendableType()) // expected-warning {{cannot access 'self' here in main actor-isolated initializer; this is an error in the Swift 6 language mode; this is an error in the Swift 6 language mode}}
   }
 
   @MainActor init?(v2_failable: Void, t: sending T) {
     self = .addressOnly(NonSendableAOStruct(value: t))
 
-    trigger()
+    trigger() // expected-note {{after this use of 'self', only main actor-isolated properties of 'self' can be accessed from this init}}
 
     _ = self.nonisoComputed
-    self = .nonSendable(NonSendableType())
+    self = .nonSendable(NonSendableType()) // expected-warning {{cannot access 'self' here in main actor-isolated initializer; this is an error in the Swift 6 language mode; this is an error in the Swift 6 language mode}}
   }
 
   @CustomActor init(v3: Void, t: sending T) {
     self = .addressOnly(NonSendableAOStruct(value: t))
 
-    trigger()
+    trigger() // expected-note {{after this use of 'self', only global actor 'CustomActor'-isolated properties of 'self' can be accessed from this init}}
 
     _ = self.nonisoComputed
-    self = .nonSendable(NonSendableType())
+    self = .nonSendable(NonSendableType()) // expected-warning {{cannot access 'self' here in global actor 'CustomActor'-isolated initializer; this is an error in the Swift 6 language mode; this is an error in the Swift 6 language mode}}
   }
 
   @CustomActor init?(v3_failable: Void, t: sending T) {
     self = .addressOnly(NonSendableAOStruct(value: t))
 
-    trigger()
+    trigger() // expected-note {{after this use of 'self', only global actor 'CustomActor'-isolated properties of 'self' can be accessed from this init}}
 
     _ = self.nonisoComputed
-    self = .nonSendable(NonSendableType())
+    self = .nonSendable(NonSendableType()) // expected-warning {{cannot access 'self' here in global actor 'CustomActor'-isolated initializer; this is an error in the Swift 6 language mode; this is an error in the Swift 6 language mode}}
   }
 }
 
@@ -207,54 +207,54 @@ enum MainActorEnumWithPayloads_AO_NC<T>: ~Copyable {
   nonisolated init(v1: Void, t: sending T) {
     self = .addressOnly(NonSendableAOStruct(value: t))
 
-    trigger()
+    trigger() // expected-note {{after calling instance method 'trigger()', only nonisolated properties of 'self' can be accessed from this init}}
 
     _ = self.nonisoComputed
-    self = .nonSendable(NonSendableType())
+    self = .nonSendable(NonSendableType()) // expected-warning {{cannot access 'self' here in nonisolated initializer; this is an error in the Swift 6 language mode; this is an error in the Swift 6 language mode}}
   }
 
   nonisolated init?(v1_failable: Void, t: sending T) {
     self = .addressOnly(NonSendableAOStruct(value: t))
 
-    trigger()
+    trigger() // expected-note {{after calling instance method 'trigger()', only nonisolated properties of 'self' can be accessed from this init}}
 
     _ = self.nonisoComputed
-    self = .nonSendable(NonSendableType())
+    self = .nonSendable(NonSendableType()) // expected-warning {{cannot access 'self' here in nonisolated initializer; this is an error in the Swift 6 language mode; this is an error in the Swift 6 language mode}}
   }
 
   @MainActor init(v2: Void, t: sending T) {
     self = .addressOnly(NonSendableAOStruct(value: t))
 
-    trigger()
+    trigger() // expected-note {{after calling instance method 'trigger()', only main actor-isolated properties of 'self' can be accessed from this init}}
 
     _ = self.nonisoComputed
-    self = .nonSendable(NonSendableType())
+    self = .nonSendable(NonSendableType()) // expected-warning {{cannot access 'self' here in main actor-isolated initializer; this is an error in the Swift 6 language mode; this is an error in the Swift 6 language mode}}
   }
 
   @MainActor init?(v2_failable: Void, t: sending T) {
     self = .addressOnly(NonSendableAOStruct(value: t))
 
-    trigger()
+    trigger() // expected-note {{after calling instance method 'trigger()', only main actor-isolated properties of 'self' can be accessed from this init}}
 
     _ = self.nonisoComputed
-    self = .nonSendable(NonSendableType())
+    self = .nonSendable(NonSendableType()) // expected-warning {{cannot access 'self' here in main actor-isolated initializer; this is an error in the Swift 6 language mode; this is an error in the Swift 6 language mode}}
   }
 
   @CustomActor init(v3: Void, t: sending T) {
     self = .addressOnly(NonSendableAOStruct(value: t))
 
-    trigger()
+    trigger() // expected-note {{after calling instance method 'trigger()', only global actor 'CustomActor'-isolated properties of 'self' can be accessed from this init}}
 
     _ = self.nonisoComputed
-    self = .nonSendable(NonSendableType())
+    self = .nonSendable(NonSendableType()) // expected-warning {{cannot access 'self' here in global actor 'CustomActor'-isolated initializer; this is an error in the Swift 6 language mode; this is an error in the Swift 6 language mode}}
   }
 
   @CustomActor init?(v3_failable: Void, t: sending T) {
     self = .addressOnly(NonSendableAOStruct(value: t))
 
-    trigger()
+    trigger() // expected-note {{after calling instance method 'trigger()', only global actor 'CustomActor'-isolated properties of 'self' can be accessed from this init}}
 
     _ = self.nonisoComputed
-    self = .nonSendable(NonSendableType())
+    self = .nonSendable(NonSendableType()) // expected-warning {{cannot access 'self' here in global actor 'CustomActor'-isolated initializer; this is an error in the Swift 6 language mode; this is an error in the Swift 6 language mode}}
   }
 }
