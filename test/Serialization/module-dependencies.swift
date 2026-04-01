3d8b8448339054e57c7aee016ee84287009bfe33
Adrian Prantl <aprantl@apple.com>
2026-04-01 11:27:28 -0700

    [Serialization] DisableDeserializationOfExplicitPaths in the compiler by default
    GH88239 demonstrates a problem with having this on by default in the
    compiler, because the additional discovery of dependencies can lead to
    an unexpected import of an implementationOnly dependency when
    importing a module as a dependency.
    
    This code no longer exists on the main branch.
    
    This patch adresses the issue by only turning on explicit dependency
    discovery when in LLDB, thereby reverting to the original module
    loading behavior.
    
    Fixes https://github.com/swiftlang/swift/issues/88239

--- test/Serialization/module-dependencies.swift
+++ test/Serialization/module-dependencies.swift
@@ -5,5 +5,5 @@
 // RUN: %target-swift-frontend -emit-module -o %t/a/A.swiftmodule %S/Inputs/a.swift -swift-module-file=B=%t/b/B.swiftmodule -disable-implicit-swift-modules -parse-stdlib
 
 // Not passing in path to b/B.swiftmodule -- it should be found in the INPUT block.
-// RUN: %target-swift-frontend -emit-module -o %t/Library.swiftmodule %s -swift-module-file=A=%t/a/A.swiftmodule -disable-implicit-swift-modules -parse-stdlib
+// RUN: not %target-swift-frontend -emit-module -o %t/Library.swiftmodule %s -swift-module-file=A=%t/a/A.swiftmodule -disable-implicit-swift-modules -parse-stdlib
 import A
